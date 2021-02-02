d5ebae2cef91995cb217ee9d643af8d090bd3a34
Thomas <t.danger@hotmail.fr>
2021-02-02 11:01:00 +0100

    ajout limite de connexion par IP

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,11 +3,20 @@
 const express = require ("express")
 const router = express.Router()
 
+const rateLimit = require("express-rate-limit")
+
 const passwordValidator = require("../middleware/password-validator")
 
 const userCtrl = require("../controllers/user")
 
+const loginLimiter = rateLimit({
+    windowsMs: 15 * 60 * 1000,
+    max: 100,
+    message : "Too many login for this IP, please try again after 15min"
+})
+
 router.post("/signup", passwordValidator, userCtrl.signup)
-router.post("/login", userCtrl.login)
+router.post("/login", loginLimiter, userCtrl.login)
 
 module.exports = router
+
